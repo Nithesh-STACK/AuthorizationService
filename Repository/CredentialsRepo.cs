ab41785389e7e8504c1b6d3b1a586fbe856f8064
Nithesh S <nitheshvilla@gmail.com>
2021-10-20 17:46:45 +0530

    Add project files.

---
+++ Repository/CredentialsRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthorizationService.KaniniModel;
+
+namespace AuthorizationService.Repository
+{
+    public class CredentialsRepo : ICredentialsRepo
+    {
+        private readonly stationaryContext db = new stationaryContext();
+        public List<Logintabl> GetCredentials()
+        {
+            return db.Logintabls.ToList();
+
+        }
+    }
+}
---
+++ Repository/ICredentialsRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthorizationService.KaniniModel;
+
+namespace AuthorizationService.Repository
+{
+    public interface ICredentialsRepo
+    {
+        public List<Logintabl> GetCredentials();
+    }
+}
